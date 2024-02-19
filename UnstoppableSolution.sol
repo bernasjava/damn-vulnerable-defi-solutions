3fd8361d890273eefe42e18856cbf33402c9b0db
Kamil Bernasiński <bernasinski.kamil@gmail.com>
2024-02-19 13:58:34 +0100

    add readme

--- UnstoppableSolution.sol
+++ UnstoppableSolution.sol
@@ -4,10 +4,8 @@ import "forge-std/console.sol";
 
 contract UnstoppableSolution {
 
-    string public token = "abcd";
+    function testUnstoppable() public {
 
-    function testToken() public {
-        console.log(token);
     }
 
 }
