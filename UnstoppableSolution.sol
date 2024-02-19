ced2e76cdf89a4772ab9a3045c47caae87362420
Kamil Bernasiński <bernasinski.kamil@gmail.com>
2024-02-19 13:51:08 +0100

    first commit

---
+++ UnstoppableSolution.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.13;
+
+import "forge-std/console.sol";
+
+contract UnstoppableSolution {
+
+    string public token = "abcd";
+
+    function testToken() public {
+        console.log(token);
+    }
+
+}
